2cbf0b2c7a7cc5f6da5ccc868c2f3cf9fe050fa9
Shubham Singhal <singhalshubham949@gmail.com>
2024-12-23 14:05:14 +0900

    Minor fix

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 # main.py
-from modules_folder.csv_dashboard import display_csv_dashboard
 
 from modules_folder.auth import authenticate_gmail
 from modules_folder.fetcher import fetch_email_data
@@ -20,13 +19,5 @@ if __name__ == '__main__':
         # Save to CSV and JSON
         save_to_csv_update(extracted_data)
         save_to_json(extracted_data)
-
-        # Set your CSV file path here
-        csv_file_path = './data/csvs/All_expenses_till_now_expenses.csv'
-        display_csv_dashboard(csv_file_path)
-
-        # Display the dashboard
-        display_csv_dashboard(csv_file_path)
-
     else:
         print("No JDebit data to save.")

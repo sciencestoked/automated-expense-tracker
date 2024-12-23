2d68f4405cbfa8fa9865e94b9d4c55f286d551ff
Shubham Singhal <singhalshubham949@gmail.com>
2024-12-23 14:01:02 +0900

    Minor . Edits

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 # main.py
+import streamlit as st
+from modules_folder.csv_dashboard import display_csv_dashboard
 
 from modules_folder.auth import authenticate_gmail
 from modules_folder.fetcher import fetch_email_data
@@ -19,5 +21,12 @@ if __name__ == '__main__':
         # Save to CSV and JSON
         save_to_csv_update(extracted_data)
         save_to_json(extracted_data)
+
+        # Set your CSV file path here
+        csv_file_path = './data/csvs/All_expenses_till_now_expenses.csv'
+
+        # Display the dashboard
+        display_csv_dashboard(csv_file_path)
+
     else:
         print("No JDebit data to save.")
